02aeed2f7ecb1abfeef134b0e2e3cb44c4d883be
anoskov <an@gettable.ru>
2016-04-01 19:03:12 +0300

    added producer to supervisor tree

--- src/random_int_generator_sup.erl
+++ src/random_int_generator_sup.erl
@@ -8,9 +8,6 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
 %% ===================================================================
 %% API functions
 %% ===================================================================
@@ -23,5 +20,11 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+  Producer =  {random_int_producer,
+    {random_int_producer, start_link, []},
+    permanent, 10000, worker,
+    [random_int_producer]},
+  RestartStrategy = {one_for_one, 5, 10},
+  Childrens = [Producer],
+  {ok, {RestartStrategy, Childrens}}.
 
