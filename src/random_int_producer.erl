0b5afd56dac75d2f7b04062482248192407dd2be
anoskov <an@gettable.ru>
2016-04-01 19:02:51 +0300

    added generator function to producer module

--- src/random_int_producer.erl
+++ src/random_int_producer.erl
@@ -12,27 +12,43 @@
 
 -author("anoskov").
 
-%% API
+-export([start_link/0, init/1, terminate/2]).
 
--export([start_link/0, init/1]).
+%% ===================================================================
+%% Callbacks
+%% ===================================================================
 
 start_link() ->
-  gen_server:start_link({global, server}, ?MODULE, [], []).
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init(_Args) ->
   io:format("Initializing producer server...~n"),
+  process_flag(trap_exit, true),
+  spawn_link(fun generator/0),
   {ok, dict:new()}.
 
 handle_call(_Args, _From, State) ->
   {reply, State}.
 
-handle_cast(_Args, _From, State) ->
+handle_cast(stop, State) ->
   {noreply, State}.
 
-handle_info(Info, State) ->
+handle_info(_Info, State) ->
   {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
-terminate(_Reason, _State) -> ok.
+terminate(shutdown, _State) -> ok.
+
+%% ===================================================================
+%% Functions
+%% ===================================================================
+
+generate_rand_int(LB, UB) -> LB + random:uniform(UB - LB).
+
+generator() ->
+  RandInt = generate_rand_int(10, 20),
+  io:format("Random integer: ~p~n", [RandInt]),
+  timer:sleep(5000),
+  generator().
